9d604dcdeb41cfb706eba98e4c4da4620b6b58e4
Jendrik Stelzner <jendrikstelzner@web.de>
2023-04-28 15:35:35 +0200

    Add new files.

--- update_files.sh
+++ update_files.sh
@@ -2,12 +2,16 @@
 
 files=(
 	.chktexrc
+	.config/fontconfig/conf.d/inactive-monospace-nolig.conf
+	.config/kitty/kitty.conf
 	.config/nvim/my_snippets/
+	.config/nvim/others/
 	.config/nvim/init.vim
 	.gitconfig
 	.latexmkrc
 	.ltex/
 	.mpv/
+	.ocamlinit
 	.XCompose
 	.zshrc
 )
